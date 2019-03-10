a674e0d605517422e5c67f15f60ef2f2659337e3
Sharon Zou <szou00@stuy.edu>
2019-03-10 18:13:30 -0400

    commented out everything but one test case, trying to debug

--- Classwork.java
+++ Classwork.java
@@ -26,7 +26,8 @@ public class Classwork {
     Random rand = new Random();
     int s = start;
     int x = rand.nextInt(end+1-start) + start;
-    // System.out.println("array current status: " + Arrays.toString(data));
+    System.out.println("array current status: " + Arrays.toString(data));
+    System.out.println("index that we want: " + x);
     int target = data[x];
     data[x] = data[start];
     data[start] = target;
@@ -34,13 +35,13 @@ public class Classwork {
     if (start!= end) {
       start+=1;
     }
-    // System.out.println("array current status: " + Arrays.toString(data));
-    // System.out.println("target: " + target);
+    System.out.println("array current status: " + Arrays.toString(data));
+    System.out.println("target: " + target);
     while (start != end) {
-      // System.out.println("array current status: " + Arrays.toString(data));
-      // System.out.println("end: " + end);
-      // System.out.println("start: " + start);
-      // System.out.println("length of data: " + data.length);
+      System.out.println("array current status: " + Arrays.toString(data));
+      System.out.println("end: " + end);
+      System.out.println("start: " + start);
+      System.out.println("length of data: " + data.length);
       if (data[start] > target) {
         int temp = data[end];
         data[end] = data[start];
@@ -84,59 +85,59 @@ public class Classwork {
     // System.out.println(makeAllWords(1,26));
     // System.out.println(partition(b,2,9));
 
-    int[] data1 = {10, 80, 30, 90, 40, 50, 70};
-    //System.out.println(Arrays.toString(data1));
-    int index1a = partition(data1, 0, 6);
-    System.out.println("1a: " + test(data1, index1a,0,6));
-    // System.out.println(Arrays.toString(data1) + " "+index1a+"\n");
-    // System.out.println(Arrays.toString(data1));
-
-    int index1b = partition(data1, 2, 5);
-    System.out.println("1b: " + test(data1, index1b,2,5));
-    // System.out.println(Arrays.toString(data1) + " "+index1b+"\n");
+    // int[] data1 = {10, 80, 30, 90, 40, 50, 70};
+    // //System.out.println(Arrays.toString(data1));
+    // int index1a = partition(data1, 0, 6);
+    // System.out.println("1a: " + test(data1, index1a,0,6));
+    // // System.out.println(Arrays.toString(data1) + " "+index1a+"\n");
+    // // System.out.println(Arrays.toString(data1));
+    //
+    // int index1b = partition(data1, 2, 5);
+    // System.out.println("1b: " + test(data1, index1b,2,5));
+    // // System.out.println(Arrays.toString(data1) + " "+index1b+"\n");
 
     int[] data2 = {6, 3, 7, -1, 7, 4, 10, -21};
-    //System.out.println(Arrays.toString(data2));
     int index2a = partition(data2, 2, 7);
     System.out.println("2a: " + test(data2, index2a,2,7));
+    System.out.println(Arrays.toString(data2));
 
-    int index2b = partition(data2, 1, 4);
-    System.out.println("2b: " + test(data2, index2b,1,4));
-
-    int[] data3 = {78};
-    //System.out.println(Arrays.toString(data3));
-    int index3 = partition(data3, 0, 0);
-    System.out.println("3: " + test(data3, index3,0,0));
-    //System.out.println(Arrays.toString(data3) + "\n");
-
-    int[] data4 = {3, 1};
-    int index4 = partition(data4, 0, 1);
-    //System.out.println(Arrays.toString(data4));
-    System.out.println("4: " + test(data4, index4,0,1));
-    //System.out.println(Arrays.toString(data4) + "\n");
-
-    for (int i = 0; i < 100; i++) {
-        try {
-            Random gen = new Random();
-            int[] test = new int[i * 1000 + 1];
-            for (int j = 0; j < test.length; j++) {
-                test[j] = gen.nextInt();
-            }
-            int start = Math.abs(gen.nextInt()) % test.length;
-            int end = start + Math.abs(gen.nextInt()) % (test.length - start);
-            //System.out.println(start + ", " + end);
-            //System.out.println(Arrays.toString(test));
-            int index = partition(test, start, end);
-            if (!test(test, index,start,end)) {
-              System.out.println(Arrays.toString(test));
-                System.out.print("FAILURE ON LENGTH " + (i * 1000 + 1));
-                System.exit(1);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.print("FAILURE ON LENGTH " + (i * 1000 + 1));
-            System.exit(1);
-        }
-    }
+    // int index2b = partition(data2, 1, 4);
+    // System.out.println("2b: " + test(data2, index2b,1,4));
+    //
+    // int[] data3 = {78};
+    // //System.out.println(Arrays.toString(data3));
+    // int index3 = partition(data3, 0, 0);
+    // System.out.println("3: " + test(data3, index3,0,0));
+    // //System.out.println(Arrays.toString(data3) + "\n");
+    //
+    // int[] data4 = {3, 1};
+    // int index4 = partition(data4, 0, 1);
+    // //System.out.println(Arrays.toString(data4));
+    // System.out.println("4: " + test(data4, index4,0,1));
+    // //System.out.println(Arrays.toString(data4) + "\n");
+    //
+    // for (int i = 0; i < 100; i++) {
+    //     try {
+    //         Random gen = new Random();
+    //         int[] test = new int[i * 1000 + 1];
+    //         for (int j = 0; j < test.length; j++) {
+    //             test[j] = gen.nextInt();
+    //         }
+    //         int start = Math.abs(gen.nextInt()) % test.length;
+    //         int end = start + Math.abs(gen.nextInt()) % (test.length - start);
+    //         //System.out.println(start + ", " + end);
+    //         //System.out.println(Arrays.toString(test));
+    //         int index = partition(test, start, end);
+    //         if (!test(test, index,start,end)) {
+    //             System.out.print("FAILURE ON LENGTH " + (i * 1000 + 1));
+    //             System.exit(1);
+    //         }
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //         System.out.println(e);
+    //         System.out.print("FAILURE ON LENGTH " + (i * 1000 + 1));
+    //         System.exit(1);
+    //     }
+    // }
   }
 }
