c41440cad3876faf546a4fc2fb6398718239cb27
Sharon Zou <szou00@stuy.edu>
2019-03-10 17:03:41 -0400

    commented out print statements, it works with all the test cases except the looping ones

--- Classwork.java
+++ Classwork.java
@@ -26,18 +26,21 @@ public class Classwork {
     Random rand = new Random();
     int s = start;
     int x = rand.nextInt(end+1-start) + start;
-    System.out.println("array current status: " + Arrays.toString(data));
+    // System.out.println("array current status: " + Arrays.toString(data));
     int target = data[x];
     data[x] = data[start];
     data[start] = target;
-    start+=1;
-    System.out.println("array current status: " + Arrays.toString(data));
-    System.out.println("target: " + target);
+
+    if (start!= end) {
+      start+=1;
+    }
+    // System.out.println("array current status: " + Arrays.toString(data));
+    // System.out.println("target: " + target);
     while (start != end) {
-      System.out.println("array current status: " + Arrays.toString(data));
-      System.out.println("end: " + end);
-      System.out.println("start: " + start);
-      System.out.println("length of data: " + data.length);
+      // System.out.println("array current status: " + Arrays.toString(data));
+      // System.out.println("end: " + end);
+      // System.out.println("start: " + start);
+      // System.out.println("length of data: " + data.length);
       if (data[start] > target) {
         int temp = data[end];
         data[end] = data[start];
@@ -85,11 +88,55 @@ public class Classwork {
     //System.out.println(Arrays.toString(data1));
     int index1a = partition(data1, 0, 6);
     System.out.println("1a: " + test(data1, index1a,0,6));
-    System.out.println(Arrays.toString(data1) + " "+index1a+"\n");
-    System.out.println(Arrays.toString(data1));
+    // System.out.println(Arrays.toString(data1) + " "+index1a+"\n");
+    // System.out.println(Arrays.toString(data1));
 
     int index1b = partition(data1, 2, 5);
     System.out.println("1b: " + test(data1, index1b,2,5));
-    System.out.println(Arrays.toString(data1) + " "+index1b+"\n");
+    // System.out.println(Arrays.toString(data1) + " "+index1b+"\n");
+
+    int[] data2 = {6, 3, 7, -1, 7, 4, 10, -21};
+    //System.out.println(Arrays.toString(data2));
+    int index2a = partition(data2, 2, 7);
+    System.out.println("2a: " + test(data2, index2a,2,7));
+
+    int index2b = partition(data2, 1, 4);
+    System.out.println("2b: " + test(data2, index2b,1,4));
+
+    int[] data3 = {78};
+    //System.out.println(Arrays.toString(data3));
+    int index3 = partition(data3, 0, 0);
+    System.out.println("3: " + test(data3, index3,0,0));
+    //System.out.println(Arrays.toString(data3) + "\n");
+
+    int[] data4 = {3, 1};
+    int index4 = partition(data4, 0, 1);
+    //System.out.println(Arrays.toString(data4));
+    System.out.println("4: " + test(data4, index4,0,1));
+    //System.out.println(Arrays.toString(data4) + "\n");
+
+    for (int i = 0; i < 100; i++) {
+        try {
+            Random gen = new Random();
+            int[] test = new int[i * 1000 + 1];
+            for (int j = 0; j < test.length; j++) {
+                test[j] = gen.nextInt();
+            }
+            int start = Math.abs(gen.nextInt()) % test.length;
+            int end = start + Math.abs(gen.nextInt()) % (test.length - start);
+            //System.out.println(start + ", " + end);
+            //System.out.println(Arrays.toString(test));
+            int index = partition(test, start, end);
+            if (!test(test, index,start,end)) {
+              System.out.println(Arrays.toString(test));
+                System.out.print("FAILURE ON LENGTH " + (i * 1000 + 1));
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.print("FAILURE ON LENGTH " + (i * 1000 + 1));
+            System.exit(1);
+        }
+    }
   }
 }
